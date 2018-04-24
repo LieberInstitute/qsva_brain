aae9d24c9b1b8667a8d256f0aeb7a1ef08194ff7
Amy Peterson <apeter65@jhu.edu>
2018-04-24 07:09:47 -0400

    Modified script for DEqual plots previously made for main and interaction models for HIPPO and DLPFC

--- brainseq_phase2_qsv/casectrl_DLPFC_plots.R
+++ brainseq_phase2_qsv/casectrl_DLPFC_plots.R
@@ -3,32 +3,52 @@
 ## degradeStats from main model, mod = model.matrix(~pd$DegradationTime + pd$Region + factor(pd$BrNum)
 ## degradeInt from interaction model, modInt = model.matrix(~pd$DegradationTime*pd$Region + factor(pd$BrNum))
 load("../ERs/rdas/DLPFC_Plus_HIPPO_RiboZero_geneLevel_degradationStats_forDEqual_hg38.rda", verbose = TRUE)
-## Load outGene, outGene0
+## Load outGene, outGene0, outGeneNoAdj
 load("rdas/dxStats_dlpfc_filtered_qSVA_geneLevel.rda", verbose = TRUE)
+## load degradeStats_DLPFC, degradeStats_HIPPO from model.matrix( ~ DegradationTime + factor(BrNum)))
+load("/dcl01/ajaffe/data/lab/qsva_brain/ERs/rdas/DLPFC_HIPPO_degradationStats_hg38.rda", verbose = TRUE)
 
-plot_dequal <- function(out_input, degrade_input, var = 't', xlabtxt = 'case-control') {
+plot_dequal <- function(out_input, degrade_input, var = 't', xlabtxt = 'case-control', main) {
 	both <- intersect(rownames(out_input), rownames(degrade_input))
 	degrade <- degrade_input[both, ]
 	interest <- out_input[both, ]
 	
 	stopifnot(identical(rownames(degrade), rownames(interest)))
-	plot(y = degrade[, var], x = interest[, var], xlab = paste(ifelse(var == 't', 't-statistic', 'log2 FC'), xlabtxt), ylab = paste(var, 'degradation'), main = signif(cor( degrade[, var],  interest[, var]), 3))
+	corr = signif(cor( degrade[, var],  interest[, var]), 3)
+	plot(y = degrade[, var], x = interest[, var], xlab = paste(ifelse(var == 't', 't-statistic', 'log2 FC'), xlabtxt), ylab = paste(var, 'degradation'), main = main)
+	legend('topleft', legend = paste('r =', corr))
 }
 
 pdf('pdf/DLPFC_dequal_mainmodel.pdf', useDingbats = FALSE)
-plot_dequal(out_input = outGene0, degrade_input = degradeStats, var = 't', xlabtxt = 'case-control (without qSVs)')
-plot_dequal(out_input = outGene, degrade_input = degradeStats, var = 't', xlabtxt = 'case-control (with qSVs)')
+plot_dequal(out_input = outGene0, degrade_input = degradeStats, var = 't', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Main Model")
+plot_dequal(out_input = outGene, degrade_input = degradeStats, var = 't', xlabtxt = 'case-control (with qSVs)', main = "DLPFC Main Model")
 
-plot_dequal(out_input = outGene0, degrade_input = degradeStats, var = 'logFC', xlabtxt = 'case-control (without qSVs)')
-plot_dequal(out_input = outGene, degrade_input = degradeStats, var = 'logFC', xlabtxt = 'case-control (with qSVs)')
+plot_dequal(out_input = outGene0, degrade_input = degradeStats, var = 'logFC', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Main Model")
+plot_dequal(out_input = outGene, degrade_input = degradeStats, var = 'logFC', xlabtxt = 'case-control (with qSVs)', main = "DLPFC Main Model")
 dev.off()
 
-pdf('pdf/DLPFC_dequal_intmodel.pdf', useDingbats = FALSE)
-plot_dequal(out_input = outGene0, degrade_input = degradeStatsInt, var = 't', xlabtxt = 'case-control (without qSVs)')
-plot_dequal(out_input = outGene, degrade_input = degradeStatsInt, var = 't', xlabtxt = 'case-control (with qSVs)')
 
-plot_dequal(out_input = outGene0, degrade_input = degradeStatsInt, var = 'logFC', xlabtxt = 'case-control (without qSVs)')
-plot_dequal(out_input = outGene, degrade_input = degradeStatsInt, var = 'logFC', xlabtxt = 'case-control (with qSVs)')
+pdf('pdf/DLPFC_dequal_region.pdf', useDingbats = FALSE)
+plot_dequal(out_input = outGene0, degrade_input = degradeStats_DLPFC, var = 't', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Region-specific Main Model")
+plot_dequal(out_input = outGene, degrade_input = degradeStats_DLPFC, var = 't', xlabtxt = 'case-control (with qSVs)', main = "DLPFC Region-specific Main Model")
+
+plot_dequal(out_input = outGene0, degrade_input = degradeStats_DLPFC, var = 'logFC', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Region-specific Main Model")
+plot_dequal(out_input = outGene, degrade_input = degradeStats_DLPFC, var = 'logFC', xlabtxt = 'case-control (with qSVs)', main = "DLPFC Region-specific Main Model")
+dev.off()
+
+
+pdf('pdf/DLPFC_dequal_dx.pdf', useDingbats = FALSE)
+plot_dequal(out_input = outGeneNoAdj, degrade_input = degradeStats_DLPFC, var = 't', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Dx Model")
+plot_dequal(out_input = outGeneNoAdj, degrade_input = degradeStats_DLPFC, var = 'logFC', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Dx Model")
+dev.off()
+
+
+pdf('pdf/DLPFC_dequal_intmodel.pdf', useDingbats = FALSE)
+plot_dequal(out_input = outGene0, degrade_input = degradeStatsInt, var = 't', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Interaction Model")
+plot_dequal(out_input = outGene, degrade_input = degradeStatsInt, var = 't', xlabtxt = 'case-control (with qSVs)', main = "DLPFC Interaction Model")
+
+plot_dequal(out_input = outGene0, degrade_input = degradeStatsInt, var = 'logFC', xlabtxt = 'case-control (without qSVs)', main = "DLPFC Interaction Model")
+plot_dequal(out_input = outGene, degrade_input = degradeStatsInt, var = 'logFC', xlabtxt = 'case-control (with qSVs)', main = "DLPFC Interaction Model")
 dev.off()
 
 
